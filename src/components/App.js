2bb1ff36fe925273390b0736218ebe724ecd2f97
Moises Diaz <Moises.diaz9108@gmail.com>
2024-04-09 16:39:24 -0400

    lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React from "react"
+import { useState, useEffect } from "react"
+
+function App() {
+
+    const [dogPic, setDogPic] = useState(null)
+
+useEffect(()=>{
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(res => res.json())
+    .then((data)=>{
+        setDogPic(data.message);
+    });
+},[])
+
+//use an empty dependencies array, so we only run the fetch request ONCE
+
+if(!dogPic) return <p>Loading...</p>
+    return (<>
+    <img src={dogPic} alt="A Random Dog"></img>
+    </>
+    )
+}
+
+export default App
